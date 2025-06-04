b777c53b5632162beb90ae83eb186cad16bd6158
Dmitrii1404 <dima1404sidorov@yandex.ru>
2025-06-04 20:52:49 +0300

    feat(ArticleParamsForm): add form functionality

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,25 +1,26 @@
-import { useState } from 'react';
 import clsx from 'clsx';
 import arrow from 'src/images/arrow.svg';
-
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	onClick?: OnClick;
+	state?: boolean;
+}
+export const ArrowButton = ({ onClick, state }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
-		>
+			onClick={onClick}
+			className={clsx(styles.container, state && styles.container_open)}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				className={clsx(styles.arrow, state && styles.arrow_open)}
 			/>
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,20 +2,138 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import {ArticleStateType, defaultArticleState, fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, OptionType} from 'src/constants/articleProps';
+import {FormEvent, MouseEventHandler, useRef, useState, useEffect} from "react";
+import clsx from "clsx";
+import {Text} from "components/text";
+import {Select} from "components/select";
+import {RadioGroup} from "components/radio-group";
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	setArticleState: (state: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = ({
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [formHide, setFormHide] = useState<boolean>(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const formRef = useRef<HTMLFormElement | null>(null);
+
+	useEffect(() => {
+		if (formHide) {
+			document.addEventListener('mousedown', handleOutsideClick);
+		} else {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, [formHide]);
+
+	const toggleFormHide = () => {
+		setFormHide((prev) => !prev);
+	};
+
+	const handleOutsideClick = (event: MouseEvent) => {
+		if (formRef.current && !formRef.current.contains(event.target as Node)) {
+			setFormHide(false);
+		}
+	};
+
+	const changeFormState = (
+		value: string,
+		field: keyof ArticleStateType,
+		options: OptionType[]
+	) => {
+		const currentOptions = options.find((op) => op.value === value);
+		setFormState((prev) => ({ ...prev, [field]: currentOptions }));
+	};
+
+	const setDefaultOptions = () => {
+		setFormState(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+
+	const applyFormOptions = (event: FormEvent) => {
+		event.preventDefault();
+		setArticleState(formState);
+		setFormHide(false);
+	};
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton onClick={toggleFormHide} state={formHide} />
 			<aside
-				className={styles.container}>
-				<form className={styles.form}>
+				className={clsx(styles.container, {
+					[styles['container_open']]: formHide,
+				})}>
+				<form className={styles.form} ref={formRef}>
+					<div className={styles.textContainer}>
+						<Text as='h1' size={31} weight={800} uppercase={true}>
+							Задайте параметры
+						</Text>
+					</div>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(event: OptionType) =>
+							changeFormState(event.value, 'fontFamilyOption', fontFamilyOptions)
+						}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-size'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(event) =>
+							changeFormState(event.value, 'fontSizeOption', fontSizeOptions)
+						}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(event: OptionType) =>
+							changeFormState(event.value, 'fontColor', fontColors)
+						}
+					/>
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(event: OptionType) =>
+							changeFormState(event.value, 'backgroundColor', backgroundColors)
+						}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(event: OptionType) =>
+							changeFormState(event.value, 'contentWidth', contentWidthArr)
+						}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
 							type='reset'
+							onClick={setDefaultOptions}
 						/>
-						<Button title='Применить' type='submit' />
+						<Button
+							title='Применить'
+							type='submit'
+							onClick={applyFormOptions}
+						/>
 					</div>
 				</form>
 			</aside>
